3d1177ad80711af607ef118d33f3109cad6a52e3
Michael Quigley <michaelforrquigley@gmail.com>
2020-12-08 22:16:46 -0800

    Add registry link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import ScrollContainer from 'react-indiana-drag-scroll';
-
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import Modal from 'react-bootstrap/Modal';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavItem from 'react-bootstrap/NavItem';
 
 import ocean_jpg from './images/ocean_pacific_grove.jpg';
 
@@ -22,59 +13,10 @@ class Description extends Component {
   render() {
     return (
         <div>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-      <p>HiieHiieHiieHiieHiieHiieHiieHiie</p>
-        </div>
+      <p>Hi All!</p>
+      <hr/>
+      We got married in a small ceremony on December 10th, 2020.
+      </div>
     )
   }
 }
@@ -82,45 +24,48 @@ class Description extends Component {
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {display_contact_info : false}
+    this.state = {display_contact_info : false, display_registry_info : false}
   }
 
   render() {
     return (
     <div className="App">
       <div className="bg" style={{ backgroundImage: `url(${ocean_jpg})`}}>
-      <Navbar className="color-nav" variant="light" expand="lg">
-        <Navbar.Brand href="#home">Maria & Michael</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#registry">Registry</Nav.Link>
-            <Nav.Link onClick={() => this.openContactInfo()}>Contact Us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Modal className="contact-modal" show={this.state.display_contact_info} onHide={() => this.closeContactInfo()}>
-      <Modal.Header closeButton>
-        <Modal.Title>Contact Info</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>Maria: mariamccauley27@gmail.com</p>
-        <p>Michael: michaelforrquigley@gmail.com</p>
-      </Modal.Body>
-      </Modal>
+        <Navbar className="color-nav" variant="light" expand="lg">
+          <Navbar.Brand href="#home">Maria & Michael</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link onClick={() => this.openRegistryInfo()}>Registry</Nav.Link>
+              <Nav.Link onClick={() => this.openContactInfo()}>Contact Info</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-      <ScrollContainer fluid="md" className="scroll" horizontal={false}>
-        <Row>
-          <Col/>
-          <Col xs={10}>
-          <Jumbotron bsPrefix="main-text">
-            <h1>Hi all!</h1>
-            <Description/>
-          </Jumbotron>
-          </Col>
-          <Col/>
-        </Row>
-      </ScrollContainer>
+        <Modal className="contact-modal"
+            show={this.state.display_contact_info} onHide={() => this.closeContactInfo()}>
+          <Modal.Header closeButton>
+            <Modal.Title>Contact Info</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Maria: mariamccauley27@gmail.com</p>
+            <p>Michael: michaelforrquigley@gmail.com</p>
+          </Modal.Body>
+        </Modal>
+
+        <Modal className="registry-modal"
+            show={this.state.display_registry_info} onHide={() => this.closeRegistryInfo()}>
+          <Modal.Header closeButton>
+            <Modal.Title>Registry</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          We are registered at: <a href="https://www.amazon.com/wedding/michael-quigley-maria-mccauley--december-2020/registry/107KFD7EQ1118">Amazon</a>
+          </Modal.Body>
+        </Modal>
+
+        <div className="main-text">
+        <Description/>
+        </div>
 
       </div>
     </div>
@@ -134,4 +79,13 @@ export default class App extends Component {
   closeContactInfo() {
     this.setState({display_contact_info : false});
   }
+
+  openRegistryInfo() {
+    this.setState({display_registry_info : true});
+  }
+
+  closeRegistryInfo() {
+    this.setState({display_registry_info : false});
+  }
+
 }
